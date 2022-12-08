39ba2590ae45040e3946a4adeafdb17a08ef6fe5
blackEye <69810869+xrwy@users.noreply.github.com>
2022-12-08 18:57:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Products from './pages/Products/Products';
@@ -26,15 +26,15 @@ function App() {
           <Route path='/' exact element={<Products />} />
           <Route path='/products' element={<Products />} />
           <Route path='/products/:product_id' element={<ProductDetail />} />
-          <Route path='/login' element={!user ? <Login /> : <Products /> } />
-          <Route path='/register' element={!user ? <Register /> : <Products /> } />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to="/" replace /> } />
+          <Route path='/register' element={!user ? <Register /> : <Navigate to="/" replace /> } />
           <Route path='/basket' element={<Basket />} />
-          <Route path='/basket/checkout' element={!user ? <Login /> : <CartCheckout />} />
+          <Route path='/basket/checkout' element={!user ? <Navigate to="/login" replace /> : <CartCheckout />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </div>
 
-      <Footer />
+      <Footer />/basket/checkout
     </Router>
   );
 }
